4959490edb2913f6639e139ab01133fa031da592
Lenoir Nicolas <nicolas.lenoir52@sfr.fr>
2021-12-14 18:47:36 +0100

    End of exercice succesfully

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const Product = require('./models/Product');
 const mongoose = require('mongoose');
 
+//Connexion à la base de données
 mongoose.connect('mongodb+srv://panzer:mpe3zQ2UWrGfRZD@nicoserver.10il5.mongodb.net/NicoServer?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
@@ -12,7 +13,8 @@ mongoose.connect('mongodb+srv://panzer:mpe3zQ2UWrGfRZD@nicoserver.10il5.mongodb.
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
-//règles de sécurité
+
+//Contrôle d'accès
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -26,41 +28,41 @@ app.use(express.json());
 
 
 app.use(bodyParser.json());
- 
+
+//Créer un article 
   app.post('/api/products', (req, res, next) =>{
     delete req.body.__v;
     const product = new Product({
- 
-        name: 'Truc et Machin',
-        description: 'poissons voraces',
-        price: '12',
-        inStock: 'true'
- 
+    ...req.body
     });
+	
     product.save()
     .then(product => res.status(201).json({ product}))
     .catch(error => res.status(400).json({ error }));
     
   });
- 
+
+
+  //Modifier un article
   app.put('/api/products/:id', (req, res, next) => {
   Product.updateOne({ _id: req.params.id }, {
-    name: 'Mon deuxieme produit',
-    description: 'produit petit',
-    price: '10',
-    inStock: 'True', _id: req.params.id })
+    ...req.body})
   .then(()=> res.status(200).json({ message: 'Objet modified !'}))
   .catch(error => res.status(404).json({ error }));
-  
+
   });
- 
+
+
+  //Supprimmer un article
   app.delete('/api/products/:id', (req, res, next) => {
   Product.deleteOne({ _id: req.params.id })
   .then(() => res.status(200).json({ message: 'Deleted !'}))
   .catch(error => res.status(404).json({ error }));
-  
+
   });
   
+
+  //Consulter un article
   app.get('/api/products/:id', (req, res, next) => {
     console.log(req.params.id);
     Product.findOne({ _id: req.params.id })
@@ -68,7 +70,8 @@ app.use(bodyParser.json());
     .catch(error => res.status(404).json({ error }));
     
   });
- 
+
+  //Consulter tous les articles
   app.get('/api/products', (req, res, next) =>{
     
     Product.find()
